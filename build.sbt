64947ab0562d9a68563a22179045f26237772eab
idursun <ibrahim.dursun@gmail.com>
2014-10-16 10:36:41 +0300

    improve example code

--- build.sbt
+++ build.sbt
@@ -1,10 +1,10 @@
-name := "scraper"
+name := "scala-akka-seed"
 
 version := "1.0"
 
 scalaVersion := "2.10.4"
 
-resolvers += "akka" at "http://repo.akka.io/snapshots"
+resolvers += "Akka Snapshots" at "http://repo.akka.io/snapshots"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.4-SNAPSHOT" withSources()
--- src/main/scala/Program.scala
+++ src/main/scala/Program.scala
@@ -1,14 +1,36 @@
 import akka.actor.{ActorLogging, Props, ActorSystem, Actor}
+import akka.routing.FromConfig
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
 sealed trait Command
 object Initialized extends Command
+case class Work(part:Int) extends Command
+case class Completed(part:Int, duration: Long) extends Command
 
-class SimpleActor extends Actor with ActorLogging {
+class SupervisorActor extends Actor with ActorLogging {
+
+  val router = context.actorOf(FromConfig.props(Props[WorkerActor]), "router")
+
   def receive = {
-    case Initialized => log.debug("initialized")
+    case Initialized => for(i <- 1 to 50) router ! Work(i)
+    case Completed(part, duration) => log.debug(s"completing work $part took $duration milliseconds")
   }
+
+}
+
+class WorkerActor extends Actor with ActorLogging {
+
+  final val rnd = new scala.util.Random()
+
+  def receive: Receive = {
+    case Work(part) =>
+      log.debug(s"working on part $part")
+      val duration: Long = rnd.nextInt(1000)
+      Thread.sleep(duration)
+      sender ! Completed(part, duration)
+  }
+
 }
 
 object Program {
@@ -16,13 +38,11 @@ object Program {
   def main(args: Array[String]) {
 
     val system = ActorSystem("system")
-    val actor = system.actorOf(Props[SimpleActor], "simple1")
+    val actor = system.actorOf(Props[SupervisorActor], "supervisor")
 
     actor ! Initialized
 
-    system.terminate()
-
-    Await.result(system.whenTerminated, 2 seconds)
+    //system.terminate()
 
   }
 
